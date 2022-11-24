2c838c6da9ec73b16754d52406f792950dc18e36
Rajgupta7080 <rajgupta09875@gmail.com>
2022-11-24 20:26:53 +0530

    login-signup added

--- Configs/db.js
+++ Configs/db.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
-const connection=async()=>{
-   try{
-    await mongoose.connect("mongodb+srv://saktiprasad:Sakti2000@backend2000.kmjifsg.mongodb.net/netmeds?retryWrites=true&w=majority")
-    console.log("DB CONNECTION ESTABLISHED")
-   }catch(err){
-        console.log(err)
-   }
+const connection = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://saktiprasad:Sakti2000@backend2000.kmjifsg.mongodb.net/netmeds?retryWrites=true&w=majority',
+    )
+    console.log('DB CONNECTION ESTABLISHED')
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export default connection
--- index.js
+++ index.js
@@ -3,19 +3,23 @@ import connection from './Configs/db.js'
 import cors from 'cors'
 import { cartProduct } from './Controller/cartcontroller.js'
 import { Products } from './Controller/productcontroller.js'
-
+import { Signup, Login } from './Controller/userRegistrationController.js'
 
 const app = express()
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 
-app.get('/cart',cartProduct)
-app.get('/product',Products)
+// Post signup login data
+app.post('/signup', Signup)
+app.post('/login', Login)
 
-app.listen(8080,()=>{
-    try{
-        connection();
-    }catch(e){
-        console.log(e)
-    }
-} )
+app.get('/cart', cartProduct)
+app.get('/product', Products)
+
+app.listen(8080, () => {
+  try {
+    connection()
+  } catch (e) {
+    console.log(e)
+  }
+})
